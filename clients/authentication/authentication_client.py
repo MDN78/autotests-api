49d0ff9c774fc57353e5d4eca41e9cfeb5065e3a
Dimitri Maksimov <salesakk@gmail.com>
2025-10-12 17:19:00 +0300

    Реализуем private builders

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -1,8 +1,16 @@
 from typing import TypedDict
-
 from httpx import Response
-
 from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client
+
+
+class Token(TypedDict):
+    """
+    Описание структуры аутентификационных bearer токенов.
+    """
+    tokenType: str
+    accessToken: str
+    refreshToken: str
 
 
 class LoginRequestDict(TypedDict):
@@ -11,6 +19,13 @@ class LoginRequestDict(TypedDict):
     """
     email: str
     password: str
+
+
+class LoginResponseDict(TypedDict):
+    """
+    Описание структуры ответа аутентификации.
+    """
+    token: Token
 
 
 class RefreshRequestDict(TypedDict):
@@ -42,3 +57,23 @@ class AuthenticationClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/authentication/refresh", json=request)
+
+    def login(self, request: LoginRequestDict) -> LoginResponseDict:
+        """
+        Method for authentification
+
+        :param request: Dict  with tokenType, accessToken, refreshToken.
+        :return: response JSON format
+        """
+        response = self.login_api(request)  # sent request for authentification
+        return response.json()  # get JSON from response
+
+
+# builder for AuthenticationClient
+def get_authentication_client() -> AuthenticationClient:
+    """
+    Функция создаёт экземпляр AuthenticationClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию AuthenticationClient.
+    """
+    return AuthenticationClient(client=get_public_http_client())
--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -1,6 +1,7 @@
 from typing import TypedDict
 from httpx import Response
 from clients.api_client import APIClient
+from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
 
 
 class CreateFileRequestDict(TypedDict):
@@ -47,3 +48,13 @@ class FilesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/files/{file_id}")
+
+
+# builder для FilesClient
+def get_files_client(user: AuthenticationUserDict) -> FilesClient:
+    """
+    Функция создаёт экземпляр FilesClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию FilesClient.
+    """
+    return FilesClient(client=get_private_http_client(user))
