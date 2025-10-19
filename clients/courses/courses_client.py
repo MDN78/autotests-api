32ed79c8d3095d7523bee2827c9f781d75a428ff
Dimitri Maksimov <salesakk@gmail.com>
2025-10-19 18:45:03 +0300

    Реализуем private builders

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -2,6 +2,22 @@ from typing import TypedDict
 from httpx import Response
 from clients.api_client import APIClient
 from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
+from clients.files.files_client import File
+from clients.users.private_users_client import User
+
+
+class Course(TypedDict):
+    """
+    Описание структуры курса.
+    """
+    id: str
+    title: str
+    maxScore: int
+    minScore: int
+    description: str
+    previewFile: File  # Вложенная структура файла
+    estimatedTime: str
+    createdByUser: User  # Вложенная структура пользователя
 
 
 class GetCoursesQueryDict(TypedDict):
@@ -22,6 +38,13 @@ class CreateCourseRequestDict(TypedDict):
     estimatedTime: str
     previewFileId: str
     createdByUserId: str
+
+
+class CreateCourseResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания курса.
+    """
+    course: Course
 
 
 class UpdateCourseRequestDict(TypedDict):
@@ -86,6 +109,17 @@ class CoursesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/courses/{course_id}")
+
+    def create_course(self, request: CreateCourseRequestDict) -> CreateCourseResponseDict:
+        """
+        Метод создания курса.
+
+        :param request: Словарь с title, maxScore, minScore, description, estimatedTime,
+        previewFileId, createdByUserId.
+        :return: Ответ от сервера в виде объекта json
+        """
+        response = self.create_course_api(request)
+        return response.json()
 
 
 # builder для CoursesClient
