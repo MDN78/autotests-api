9af260c65bfbe0d77e63ff9e3b8c775634a225a5
Dimitri Maksimov <salesakk@gmail.com>
2025-10-26 17:25:32 +0300

    Фикстуры Pytest

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+import pytest
+from pydantic import BaseModel, EmailStr
+
+from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
+# Импортируем запрос и ответ создания пользователя, модель данных пользователя
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+
+
+class UserFixture(BaseModel):
+    """
+    Модель для агрегации возвращаемых данных фикстурой function_user
+    """
+    request: CreateUserRequestSchema
+    response: CreateUserResponseSchema
+
+    @property
+    def email(self) -> EmailStr:  # Быстрый доступ к email пользователя
+        return self.request.email
+
+    @property
+    def password(self) -> str:  # Быстрый доступ к password пользователя
+        return self.request.password
+
+
+@pytest.fixture
+def authentication_client() -> AuthenticationClient:
+    return get_authentication_client()
+
+
+@pytest.fixture
+def public_users_client() -> PublicUsersClient:
+    return get_public_users_client()
+
+
+@pytest.fixture
+# Используем фикстуру public_users_client, которая создает нужный API клиент
+def function_user(public_users_client: PublicUsersClient) -> UserFixture:
+    """
+    Фикстура для создания пользователя
+    :param public_users_client:
+    :return: UserFixture, request and response
+    """
+    request = CreateUserRequestSchema()
+    response = public_users_client.create_user(request)
+    return UserFixture(request=request, response=response)  # Возвращаем все нужные данные
--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,9 +1,7 @@
 from http import HTTPStatus
-from clients.authentication.authentication_client import get_authentication_client
-from clients.authentication.authentication_schema import LoginResponseSchema
-from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema
+from clients.authentication.authentication_client import AuthenticationClient
+from clients.authentication.authentication_schema import LoginResponseSchema, LoginRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -12,26 +10,15 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login():
-    # Инициализируем клиент PublicUsersClient
-    public_users_client = get_public_users_client()
-    # Создание User
-    create_user_request = CreateUserRequestSchema()
-    public_users_client.create_user(create_user_request)
-
-    authentication_user = AuthenticationUserSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
-    )
-    # Инициализируем клиент AuthenticationClient
-    auth_user_client = get_authentication_client()
+def test_login(function_user: UserFixture, authentication_client: AuthenticationClient):
     # Аутентифицируемся созданным пользователем
-    login_response = auth_user_client.login_api(authentication_user)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    response = authentication_client.login_api(request)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
     # Проверяем статус-код ответа
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_status_code(response.status_code, HTTPStatus.OK)
     # Проверяем корректность тела ответа
-    assert_login_response(login_response_data)
+    assert_login_response(response_data)
     # валидация JSON-схемы
-    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
+    validate_json_schema(response.json(), response_data.model_json_schema())
