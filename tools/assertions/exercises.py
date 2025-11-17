3ca992a3bd48e585678201fc2c0e933468e9bac1
Dimitri Maksimov <salesakk@gmail.com>
2025-11-17 19:24:32 +0300

    Создание задания

---
+++ tools/assertions/exercises.py
@@ -0,0 +1,18 @@
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from tools.assertions.base import assert_equal
+
+
+def assert_create_exercise_response(request: CreateExerciseRequestSchema, response: CreateExerciseResponseSchema):
+    """
+    Проверяет, что ответ на создание упражнения соответствует модели создания упражнения
+    :param request: Исходный запрос на создание упражнения
+    :param response: ответ API с созданным упражнением
+    :return AssertionError: Если данные упражнений не совпадают.
+    """
+    assert_equal(request.title, response.exercise.title, "title")
+    assert_equal(request.course_id, response.exercise.course_id, "course_id")
+    assert_equal(request.max_score, response.exercise.max_score, "max_score")
+    assert_equal(request.min_score, response.exercise.min_score, "min_score")
+    assert_equal(request.order_index, response.exercise.order_index, "order_index")
+    assert_equal(request.description, response.exercise.description, "description")
+    assert_equal(request.estimated_time, response.exercise.estimated_time, "estimated_time")
