bddbb662514d6abec85bf292be6b194bf96c80df
Dimitri Maksimov <salesakk@gmail.com>
2025-11-19 10:07:08 +0300

    Получение задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,5 +1,38 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    ExerciseSchema, GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверяет, что фактические данные упражнения соответствуют ожидаемым.
+
+    :param actual: Фактические данные упражнения.
+    :param expected: Ожидаемые данные упражнения.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_get_exercise_response(
+        get_exercise_response: GetExerciseResponseSchema,
+        create_exercise_response: CreateExerciseResponseSchema):
+    """
+    Проверяет, что ответ на получение упражнения содержит поле exercise и фактические
+    данные упражнения соответствуют ожидаемым.
+
+    :param get_exercise_response: Ответ API при запросе упражнения.
+    :param create_exercise_response: Список API ответов при создании упражнения.
+    :raises AssertionError: Если данные курсов не совпадают.
+    """
+    assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
 
 
 def assert_create_exercise_response(request: CreateExerciseRequestSchema, response: CreateExerciseResponseSchema):
