3f3d255400a656ab184081aa3e535adf70a11322
Dimitri Maksimov <salesakk@gmail.com>
2025-11-29 13:38:42 +0300

    Обновление задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,5 +1,5 @@
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    ExerciseSchema, GetExerciseResponseSchema
+    ExerciseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -46,6 +46,25 @@ def assert_create_exercise_response(request: CreateExerciseRequestSchema, respon
     assert_equal(request.course_id, response.exercise.course_id, "course_id")
     assert_equal(request.max_score, response.exercise.max_score, "max_score")
     assert_equal(request.min_score, response.exercise.min_score, "min_score")
+    assert_equal(request.order_index, response.exercise.order_index, "order_index")
+    assert_equal(request.description, response.exercise.description, "description")
+    assert_equal(request.estimated_time, response.exercise.estimated_time, "estimated_time")
+
+
+def assert_update_exercise_response(
+        request: UpdateExerciseRequestSchema,
+        response: UpdateExerciseResponseSchema
+):
+    """
+        Проверяет, что ответ на обновление упражнения соответствует данным из запроса.
+
+        :param request: Исходный запрос на обновление упражнения.
+        :param response: Ответ API с обновленными данными упражнения.
+        :raises AssertionError: Если хотя бы одно поле не совпадает.
+        """
+    assert_equal(request.title, response.exercise.title, "title")
+    assert_equal(request.min_score, response.exercise.min_score, "min_score")
+    assert_equal(request.max_score, response.exercise.max_score, "max_score")
     assert_equal(request.order_index, response.exercise.order_index, "order_index")
     assert_equal(request.description, response.exercise.description, "description")
     assert_equal(request.estimated_time, response.exercise.estimated_time, "estimated_time")
