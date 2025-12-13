c92743e73c1721a56bd90268ca655494d90f5550
Dimitri Maksimov <salesakk@gmail.com>
2025-12-13 18:06:39 +0300

    add swagger coverage tools

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -1,6 +1,7 @@
 import allure
 from httpx import Response
 from clients.api_client import APIClient
+from clients.api_coverage import tracker
 from clients.public_http_builder import get_public_http_client
 from clients.authentication.authentication_schema import LoginRequestSchema, RefreshRequestSchema, LoginResponseSchema
 from tools.routes import APIRoutes
@@ -12,6 +13,7 @@ class AuthenticationClient(APIClient):
     """
 
     @allure.step("Authenticate user")
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/login")
     def login_api(self, request: LoginRequestSchema) -> Response:
         """
         Метод выполняет аутентификацию пользователя.
@@ -21,6 +23,7 @@ class AuthenticationClient(APIClient):
         """
         return self.post(f"{APIRoutes.AUTHENTICATION}/login", json=request.model_dump(by_alias=True))
 
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/refresh")
     @allure.step("Refresh authentication token")
     def refresh_api(self, request: RefreshRequestSchema) -> Response:
         """
